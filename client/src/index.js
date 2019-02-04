762b1338817b92069aeb92b9d5ff6905bb303d7a
ajfrierson <alvinfrierson@fuller.edu>
2019-02-04 13:34:16 -0800

    removed redux files. They are currently not necessary.

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,7 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 
-import store from './store/store';
+// import store from './store/store';
 import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
