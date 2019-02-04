604f7318d6999f9f6323f92f54d0ef408cfe4e9a
joepound <14357797+joepound@users.noreply.github.com>
2019-02-04 12:19:41 -0700

    Redux boilerplating

---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+// Import your reducers here
+// [example] import sampleReducer from "path";
+
+export default combineReducers({
+  // Add reducers
+  // [example] reducer1, reducer2... etc.
+});
---
+++ client/src/store/reducers/sampleReducer.js
@@ -0,0 +1,23 @@
+/*
+// NOTE: Just change the name of this reducer once it's ready
+
+// Import all the named exports from types.js 
+import {
+  
+} from "./types";
+
+let initialState = {
+
+};
+
+const sampleReducer  = (state = initialState, action) => {
+  switch (action.type) {
+    // Add switch cases here
+
+    default:
+      return state;
+  }
+}
+
+export default sampleReducer;
+*/
